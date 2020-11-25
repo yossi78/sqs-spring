24640c861c92c23ab04686df2f6da8e4af5655cb
yossi78 <yossi78@gmail.com>
2020-11-25 15:34:23 +0200

    spaces

--- src/main/java/com/example/sqsspring/aws/AWSCredentials.java
+++ src/main/java/com/example/sqsspring/aws/AWSCredentials.java
@@ -5,7 +5,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 
